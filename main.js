60e5d26977324d9fd17c50d0eeb3d57467c5843e
natbell77 <35178561+natbell77@users.noreply.github.com>
2022-04-25 11:32:25 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+// my current age
+const myAge = 5;
+
+// early years, mutable
+var earlyYears = 2;
+
+// multiply early years by 10.5 and reassign value
+earlyYears *= 10.5;
+
+// subtract two from my age
+laterYears = myAge - 2;
+
+// multiply the laterYears variable by 4 and reassign
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+// calculate age in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+
+// lowercase my first name
+myName = "Nathaniel".toLowerCase();
+
+// print the complete result as a string
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
